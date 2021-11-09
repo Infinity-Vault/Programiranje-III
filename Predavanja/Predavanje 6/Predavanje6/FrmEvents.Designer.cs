337c1d3f7f162f7709c848c7df8b7984b97b1863
Haris Kordić <kordicharis18@gmail.com>
2021-11-09 17:49:49 +0100

    Add Predavanje 6

---
+++ Predavanja/Predavanje 6/Predavanje6/FrmEvents.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Predavanje6
+{
+    partial class FrmEvents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxPrikaz = new System.Windows.Forms.RichTextBox();
+            this.cbPrvaGodina = new System.Windows.Forms.CheckBox();
+            this.cbDrugaGodina = new System.Windows.Forms.CheckBox();
+            this.cbTrecaGodina = new System.Windows.Forms.CheckBox();
+            this.cbCetvrtaGodina = new System.Windows.Forms.CheckBox();
+            this.btnSendNotification = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxPrikaz
+            // 
+            this.txtBoxPrikaz.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtBoxPrikaz.Location = new System.Drawing.Point(2, 2);
+            this.txtBoxPrikaz.Name = "txtBoxPrikaz";
+            this.txtBoxPrikaz.Size = new System.Drawing.Size(436, 215);
+            this.txtBoxPrikaz.TabIndex = 0;
+            this.txtBoxPrikaz.Text = "";
+            // 
+            // cbPrvaGodina
+            // 
+            this.cbPrvaGodina.AutoSize = true;
+            this.cbPrvaGodina.Location = new System.Drawing.Point(10, 229);
+            this.cbPrvaGodina.Name = "cbPrvaGodina";
+            this.cbPrvaGodina.Size = new System.Drawing.Size(83, 17);
+            this.cbPrvaGodina.TabIndex = 1;
+            this.cbPrvaGodina.Text = "Prva godina";
+            this.cbPrvaGodina.UseVisualStyleBackColor = true;
+            this.cbPrvaGodina.CheckedChanged += new System.EventHandler(this.cbPrvaGodina_CheckedChanged);
+            // 
+            // cbDrugaGodina
+            // 
+            this.cbDrugaGodina.AutoSize = true;
+            this.cbDrugaGodina.Location = new System.Drawing.Point(95, 229);
+            this.cbDrugaGodina.Name = "cbDrugaGodina";
+            this.cbDrugaGodina.Size = new System.Drawing.Size(90, 17);
+            this.cbDrugaGodina.TabIndex = 2;
+            this.cbDrugaGodina.Text = "Druga godina";
+            this.cbDrugaGodina.UseVisualStyleBackColor = true;
+            this.cbDrugaGodina.CheckedChanged += new System.EventHandler(this.cbDrugaGodina_CheckedChanged);
+            // 
+            // cbTrecaGodina
+            // 
+            this.cbTrecaGodina.AutoSize = true;
+            this.cbTrecaGodina.Location = new System.Drawing.Point(183, 229);
+            this.cbTrecaGodina.Name = "cbTrecaGodina";
+            this.cbTrecaGodina.Size = new System.Drawing.Size(89, 17);
+            this.cbTrecaGodina.TabIndex = 3;
+            this.cbTrecaGodina.Text = "Treca godina";
+            this.cbTrecaGodina.UseVisualStyleBackColor = true;
+            this.cbTrecaGodina.CheckedChanged += new System.EventHandler(this.cbTrecaGodina_CheckedChanged);
+            // 
+            // cbCetvrtaGodina
+            // 
+            this.cbCetvrtaGodina.AutoSize = true;
+            this.cbCetvrtaGodina.Location = new System.Drawing.Point(273, 229);
+            this.cbCetvrtaGodina.Name = "cbCetvrtaGodina";
+            this.cbCetvrtaGodina.Size = new System.Drawing.Size(95, 17);
+            this.cbCetvrtaGodina.TabIndex = 4;
+            this.cbCetvrtaGodina.Text = "Cetvrta godina";
+            this.cbCetvrtaGodina.UseVisualStyleBackColor = true;
+            this.cbCetvrtaGodina.CheckedChanged += new System.EventHandler(this.cbCetvrtaGodina_CheckedChanged);
+            // 
+            // btnSendNotification
+            // 
+            this.btnSendNotification.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSendNotification.Location = new System.Drawing.Point(363, 225);
+            this.btnSendNotification.Name = "btnSendNotification";
+            this.btnSendNotification.Size = new System.Drawing.Size(75, 23);
+            this.btnSendNotification.TabIndex = 5;
+            this.btnSendNotification.Text = "Posalji";
+            this.btnSendNotification.UseVisualStyleBackColor = true;
+            this.btnSendNotification.Click += new System.EventHandler(this.btnSendNotification_Click);
+            // 
+            // FrmEvents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 258);
+            this.Controls.Add(this.btnSendNotification);
+            this.Controls.Add(this.cbCetvrtaGodina);
+            this.Controls.Add(this.cbTrecaGodina);
+            this.Controls.Add(this.cbDrugaGodina);
+            this.Controls.Add(this.cbPrvaGodina);
+            this.Controls.Add(this.txtBoxPrikaz);
+            this.Name = "FrmEvents";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplikacija";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmEvents_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmEvents_FormClosed);
+            this.Load += new System.EventHandler(this.FrmEvents_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBoxPrikaz;
+        private System.Windows.Forms.CheckBox cbPrvaGodina;
+        private System.Windows.Forms.CheckBox cbDrugaGodina;
+        private System.Windows.Forms.CheckBox cbTrecaGodina;
+        private System.Windows.Forms.CheckBox cbCetvrtaGodina;
+        private System.Windows.Forms.Button btnSendNotification;
+    }
+}
+
---
+++ Predavanja/Predavanje 6/Predavanje6/FrmEvents.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Predavanje6
+{
+    public partial class FrmEvents : Form
+    {
+        public FrmEvents()
+        {
+            InitializeComponent();
+        }
+
+
+        //Primjer  kreiranja pokazivaca na metodu:
+
+        //Kljucnom rjecju delegate oznacimo da ce potpis iza predstavljati pokazivac na metodu(e);
+        //Sintaksa: delegate povratniTip  imePokazivaca  (ulazniParametri);
+
+        private delegate void Notifikacija(string msg);//Kreirali smo "pokazivac" na bilo koju metodu koja postuje njegov potpis, znaci da je void i da prima string;
+        //Ovo je slicno kao i pokazivaci na funkcije u C++;
+
+        //Primjer kreiranja dogadjaja u koji ce se spremati svi nasi pokazivaci na metode;
+
+        //Sintaksa: event tipPokazivaca imeDogadjaja;
+        //Kljucna rijec event oznacava da ce to biti "niz" u koji ce se spremati svi pokazivaci na metode;
+
+        private event Notifikacija NovaNotifikacija;//Kreiran niz u kojem cemo spremiti sve pokazivace na metode;Pokazivaci moraju biti Notifikacija jer event "niz" sprema takve vrste pokazivaca;
+
+
+        //Metoda koja ce simulirati slanje nekih notifikacija:
+        private void btnSendNotification_Click(object sender, EventArgs e)
+        {
+            NovaNotifikacija?.Invoke(txtBoxPrikaz.Text);//Iz niza ovog eventa dobicemo sve metode kojima je proslijedjen text iz naseg text boxa sa forme;Takodjer to moze biti nesto sto je hard kodirano tipa neka poruka;
+            //Postoji mogucnost da nijedan checkBox nije oznacen, te samim time niz naseg eventa ce biti prazan,
+            //a mi mu pokusavamo pristupiti i dolazi do null reference exceptiona;
+            //Ovo se izbjegne na nacin da se prije poziva Invoke metode (koja se uvijek za svaki poziv bilo koje metoda implicitno pozove) 
+            //stavimo operator provjere da li je objekat sa lijeva null (znak ?);
+        }
+
+        //Metode za razlicite godine fakulteta:
+        private void PrvaGodina(string obavijest)
+        {
+            MessageBox.Show($"Na prvoj godini fakulteta {obavijest}");//Ispis poruke;
+        }
+        private void DrugaGodina(string obavijest)
+        {
+            MessageBox.Show($"Na drugoj godini fakulteta {obavijest}");
+        }
+        private void TrecaGodina(string obavijest)
+        {
+            MessageBox.Show($"Na trecoj godini fakulteta {obavijest}");
+        }
+        private void CetvrtaGodina(string obavijest)
+        {
+            MessageBox.Show($"Na cetvrtoj godini fakulteta {obavijest}");
+        }
+
+        //Generalna metoda za provjeru checkBoxova:
+        private void ProvjeraKomeSeSaljeNotifikacija(CheckBox checkBox, Notifikacija notifikacija)
+        {
+            //Prvi parametar znamo da ce biti checkBox dok drugi je ustvari nas delegate (pokazivac) na metodu(e);
+
+            if (checkBox.Checked)
+                NovaNotifikacija += notifikacija;//Ako je oznacena kucica dodaj u niz pokazivac na metodu;
+            else
+                NovaNotifikacija -= notifikacija;//Ako nije oznacena kucica ukloni iz niza pokazivac na metodu;
+        }
+        //Gornju metodu mozemo koristiti za sva cetiri klika na checkBox:
+        private void cbPrvaGodina_CheckedChanged(object sender, EventArgs e)
+        {
+            ProvjeraKomeSeSaljeNotifikacija(cbPrvaGodina, PrvaGodina);//Posaljemo checkBox prve godine i pok na metodu;
+        }
+        private void cbDrugaGodina_CheckedChanged(object sender, EventArgs e)
+        {
+            ProvjeraKomeSeSaljeNotifikacija(sender as CheckBox, DrugaGodina);//Mozemo i ovako poslati trenutno kliknuti checkBox;
+        }
+        private void cbTrecaGodina_CheckedChanged(object sender, EventArgs e)
+        {
+            ProvjeraKomeSeSaljeNotifikacija(sender as CheckBox, TrecaGodina);
+        }
+        private void cbCetvrtaGodina_CheckedChanged(object sender, EventArgs e)
+        {
+            ProvjeraKomeSeSaljeNotifikacija(sender as CheckBox, CetvrtaGodina);
+        }
+      //------------------------------------------------------------------------------------------------------//
+
+
+      //Drugi primjer:
+      private delegate int Operacija(int vrijednost);//Pokazivac na metodu koja vraca int i prima int;
+
+      //Kreiranje dvije operacije na koje ce se moci pokazati gornjim delegatom:
+      private int  Kvadriraj(int vrijednost) => vrijednost * vrijednost;//Expression oblik;//Isti potpis kao delegat;
+      private int Kubiraj(int vrijednost)//Isti potpis kao delegat;
+      {
+          return vrijednost * vrijednost * vrijednost;
+      }
+
+      private void Calculator(Operacija operacija, params int[] operandi)//Primimo delegat i params int;
+      {
+          var result = 0;
+
+          for (int i = 0; i < operandi.Length; i++)
+          {
+              result += operacija(operandi[i]);//Sacuvamo (dodajemo jedno na drugo) ono sto pokazivac na odredjenu metodu vrati;
+          }
+          txtBoxPrikaz.Text += result.ToString();//Prikazemo u nasem text box;
+          txtBoxPrikaz.Text += Environment.NewLine;//Dodamo novu liniju;
+      }
+
+
+        //Metoda koja ce samo kalkulator na ucitavanju pozvati:
+        private void FrmEvents_Load(object sender, EventArgs e)
+        {
+            Calculator(Kvadriraj, 2, 3, 5, 6);//Pomocu params mozemo posalti jedan int ili n njih;
+            Calculator(Kubiraj, 3, 4, 6);
+            //Primjer za Func:
+            //Prvi argument je sta se prima a drugi sta se vraca metodom;
+            Func<int, int> OperacijaZaFunc = Kvadriraj; //Kreirali smo pok na metodu koja prima int i vraca int;//Pokazuje na metodu Kvadriraj;
+            Calculator2(OperacijaZaFunc, 2, 3, 5, 6);
+            OperacijaZaFunc = Kubiraj;//Preusmjerimo na metodu kubiraj;
+            Calculator2(OperacijaZaFunc, 3, 4, 6);
+
+            //Saljemo samo ulazni tip argumenta, povratni je uvijek void;
+            Action<string> NekaPoruka=NekaMetodaIspisa;//Kreiramo pokazivac na metodu koja prima string i void je povratnog tipa;
+            NekaPoruka("Ovo je Action tip delegata koji je uvijek void povratnog tipa.");
+
+            //Yield:
+            YieldDemo();
+        }
+
+        //------------------------------------------------------------------------------------------------------//
+        //Treci primjer:
+        private event EventHandler Event;//Event se moze kreirati i ovako jer je EventHandler bazni event svima  te ce moci u svoj "niz" staviti bilo koji pok na metodu koja je void i ima parametre object i EventArgs; 
+        private void NekaFunckija(object sender, EventArgs e)
+        {
+            Event?.Invoke(sender, e);
+        }
+        
+        //Delegate mozemo kreirati i pomocu generickih klasa Func i Action;
+        //Razlika je sto Func ne moze imati povratni tip void dok Action po defaultu prima povratni tip void i to je ujedno jedini povratni tip koji moze imati;
+
+        //Primjer za Func:
+        private void Calculator2(Func<int,int> operacija, params int[] operandi)
+        {
+            var result = 0;
+
+            for (int i = 0; i < operandi.Length; i++)
+            {
+                result += operacija(operandi[i]);//Sacuvamo (dodajemo jedno na drugo) ono sto pokazivac na odredjenu metodu vrati;
+            }
+            txtBoxPrikaz.Text += result.ToString();//Prikazemo u nasem text box;
+            txtBoxPrikaz.Text += Environment.NewLine;//Dodamo novu liniju;
+        }
+
+        //Primjer za Action:
+        private void NekaMetodaIspisa(string poruka)
+        {
+            MessageBox.Show(poruka);
+        }
+
+        //Metoda koja se pokrene kada se forma zatvori:
+        private void FrmEvents_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show($"Razlog zatvaranja forme je {e.CloseReason}");
+        }
+        //Metoda koja se pokrene kada se forma krene zatvarati:
+        private void FrmEvents_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Niste jos unijeli sve podatke te forma ne moze da se zatvori!");
+            e.Cancel = false;//bool vrijednost, false nece se dati da se zatvori, true dati ce da se zatvori;
+        }
+
+
+        //Kreiranje klase Student:
+
+        internal class Student
+        {
+            //Kreiramo niz ocjena Studenta:
+            public List<int> Ocjene { get; set;} = new List<int>(){6,6,7,7,8,9,10,10,5};//Odmah ih inicijalizujemo;
+
+            public IEnumerator<int> GetEnumerator()//Svejedno je ili <int> ili samo IEnumerator jer je primitivni tip;
+            {
+                for (int i = 0; i < Ocjene.Count; i++)
+                {
+                    yield return Ocjene[i];//Kljucna rijec yield omogucava da se svakom novom iteracijom pamti prijasnja vrijednost brojaca iako se metoda iznova i iznova poziva;
+                }
+            }
+
+            public string SortirajOcjene()
+            {
+                string prolazneOcjene="";//Pocetni prazni string;
+
+                bool Sort(int ocjena)//Metoda koja sortira ocjene;
+                {
+                    if(ocjena>=6)//ako je prolazna vrati true ako nije vrati false;
+                        return true;
+                    return false;
+                }
+                var prolazne=Ocjene.Where(Sort);//Spremimo niz prolaznih ocj u  novu varijablu;
+                foreach (var ocjena in prolazne)
+                {
+                    prolazneOcjene += $"{ocjena},";//Svaku ocjenu spremimo u string;
+                }
+                return prolazneOcjene;//Vratimo string;
+            }
+        }
+
+        //Metoda koja ce demonstrirati svrhu yield:
+        private void YieldDemo()
+        {
+            var student = new Student();//Kreiramo nekog default studenta;
+            foreach (var ocjena in student)//Iteriramo kroz sve studente,medjutim implementirali smo GetEnumerator metodu IEnumerator interfejsa koja ce nam omoguciti da student moze biti numerisan na nacin da  ta metoda vrati sve ocjene tog studenta;U suprotnom ne mozemo foreach imati na student; GetEnumerator mora biti public kako bi se moglo pristupiti u foreach;
+                txtBoxPrikaz.Text += $"{ocjena},";//Ispisemo ocjenu;
+            txtBoxPrikaz.Text += Environment.NewLine;//Novi red;
+            txtBoxPrikaz.Text += $"Prolazne ocjene su:{Environment.NewLine}";
+            txtBoxPrikaz.Text+=$"{student.SortirajOcjene()}{Environment.NewLine}";//Ispisemo prolazne ocjene;
+        }
+    }
+}
