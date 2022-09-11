c4ed0743fb463a70492014197192bee60620e360
Adi <efendic100@gmail.com>
2022-09-11 18:37:01 +0200

    Dodaj 01.09.2022 ispitni

---
+++ Ispitni/2022-09-01/DLWMS.WinForms/IB200290/frmNovaPorukaIB200290.cs
@@ -0,0 +1,72 @@
+﻿using DLWMS.WinForms.DB;
+using DLWMS.WinForms.Helpers;
+using DLWMS.WinForms.P5;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinForms.IB200290
+{
+    public partial class frmNovaPorukaIB200290 : Form
+    {
+        KonekcijaNaBazu bp = DLWMSdb.Baza;
+        private Student odabraniStudent;
+
+        public frmNovaPorukaIB200290()
+        {
+            InitializeComponent();
+        }
+
+        public frmNovaPorukaIB200290(Student odabraniStudent) : this()
+        {
+            this.odabraniStudent = odabraniStudent;
+        }
+
+        private void frmNovaPorukaIB200290_Load(object sender, EventArgs e)
+        {
+            cBPredmet.DataSource = bp.Predmeti.ToList();
+        }
+
+        private void btnSacuvaj_Click(object sender, EventArgs e)
+        {
+            if(Validator.ValidirajKontrolu(cBPredmet,err,Poruke.ObaveznaVrijednost)
+                && Validator.ValidirajKontrolu(txtSadrzaj,err,Poruke.ObaveznaVrijednost))
+            {
+                var novaPoruka = new StudentPoruka()
+                {
+                    Predmet = cBPredmet.SelectedItem as Predmet,
+                    Sadrzaj = txtSadrzaj.Text,
+                    Slika = (pBSlika.Image != null ? ImageHelper.FromImageToByte(pBSlika.Image) : null),
+                    Student = odabraniStudent,
+                    VrijemeKreiranja = DateTime.Now
+                };
+                bp.StudentiPoruke.Add(novaPoruka);
+                bp.SaveChanges();
+                MessageBox.Show("Uspjesno ste dodali poruku za odabranog studenta!");
+                cBPredmet.SelectedIndex = 0;
+                txtSadrzaj.Text = "";
+                pBSlika.Image = null;
+            }
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pBSlika_Click(object sender, EventArgs e)
+        {
+            var slika = new OpenFileDialog();
+            if (slika.ShowDialog() == DialogResult.OK) 
+            {
+                pBSlika.Image = Image.FromFile(slika.FileName);
+            }
+        }
+    }
+}
---
+++ Ispitni/2022-09-01/DLWMS.WinForms/IB200290/frmPorukeIB200290.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace DLWMS.WinForms.IB200290
+{
+    partial class frmPorukeIB200290
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.btnNovaPoruka = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.predmetDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sadrzajDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.slikaDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Brisi = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.studentPorukaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnPrintaj = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbPredmet = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBrojPoruka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentPorukaBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Poruke studenta";
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Location = new System.Drawing.Point(100, 11);
+            this.lblStudent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(59, 13);
+            this.lblStudent.TabIndex = 1;
+            this.lblStudent.Text = "STUDENT";
+            // 
+            // btnNovaPoruka
+            // 
+            this.btnNovaPoruka.Location = new System.Drawing.Point(652, 10);
+            this.btnNovaPoruka.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNovaPoruka.Name = "btnNovaPoruka";
+            this.btnNovaPoruka.Size = new System.Drawing.Size(82, 23);
+            this.btnNovaPoruka.TabIndex = 2;
+            this.btnNovaPoruka.Text = "Nova Poruka";
+            this.btnNovaPoruka.UseVisualStyleBackColor = true;
+            this.btnNovaPoruka.Click += new System.EventHandler(this.btnNovaPoruka_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.predmetDataGridViewTextBoxColumn,
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn,
+            this.sadrzajDataGridViewTextBoxColumn,
+            this.slikaDataGridViewImageColumn,
+            this.Brisi});
+            this.dataGridView1.DataSource = this.studentPorukaBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 43);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 232);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // predmetDataGridViewTextBoxColumn
+            // 
+            this.predmetDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.predmetDataGridViewTextBoxColumn.DataPropertyName = "Predmet";
+            this.predmetDataGridViewTextBoxColumn.HeaderText = "Predmet";
+            this.predmetDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.predmetDataGridViewTextBoxColumn.Name = "predmetDataGridViewTextBoxColumn";
+            this.predmetDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // vrijemeKreiranjaDataGridViewTextBoxColumn
+            // 
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.DataPropertyName = "VrijemeKreiranja";
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.HeaderText = "Vrijeme";
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.Name = "vrijemeKreiranjaDataGridViewTextBoxColumn";
+            this.vrijemeKreiranjaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // sadrzajDataGridViewTextBoxColumn
+            // 
+            this.sadrzajDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.sadrzajDataGridViewTextBoxColumn.DataPropertyName = "Sadrzaj";
+            this.sadrzajDataGridViewTextBoxColumn.HeaderText = "Sadrzaj";
+            this.sadrzajDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.sadrzajDataGridViewTextBoxColumn.Name = "sadrzajDataGridViewTextBoxColumn";
+            this.sadrzajDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // slikaDataGridViewImageColumn
+            // 
+            this.slikaDataGridViewImageColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.slikaDataGridViewImageColumn.DataPropertyName = "Slika";
+            this.slikaDataGridViewImageColumn.HeaderText = "Slika";
+            this.slikaDataGridViewImageColumn.MinimumWidth = 6;
+            this.slikaDataGridViewImageColumn.Name = "slikaDataGridViewImageColumn";
+            this.slikaDataGridViewImageColumn.ReadOnly = true;
+            // 
+            // Brisi
+            // 
+            this.Brisi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Brisi.HeaderText = "";
+            this.Brisi.MinimumWidth = 6;
+            this.Brisi.Name = "Brisi";
+            this.Brisi.ReadOnly = true;
+            this.Brisi.Text = "Brisi";
+            this.Brisi.ToolTipText = "Brisi";
+            this.Brisi.UseColumnTextForButtonValue = true;
+            // 
+            // studentPorukaBindingSource
+            // 
+            this.studentPorukaBindingSource.DataSource = typeof(DLWMS.WinForms.IB200290.StudentPoruka);
+            // 
+            // btnPrintaj
+            // 
+            this.btnPrintaj.Location = new System.Drawing.Point(652, 292);
+            this.btnPrintaj.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPrintaj.Name = "btnPrintaj";
+            this.btnPrintaj.Size = new System.Drawing.Size(82, 23);
+            this.btnPrintaj.TabIndex = 4;
+            this.btnPrintaj.Text = "Printaj";
+            this.btnPrintaj.UseVisualStyleBackColor = true;
+            this.btnPrintaj.Click += new System.EventHandler(this.btnPrintaj_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDodaj);
+            this.groupBox1.Controls.Add(this.txtInfo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbPredmet);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBrojPoruka);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 292);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(636, 165);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dodavanje poruka";
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(35, 121);
+            this.btnDodaj.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(82, 23);
+            this.btnDodaj.TabIndex = 6;
+            this.btnDodaj.Text = "Dodaj =>";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(200, 42);
+            this.txtInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(433, 119);
+            this.txtInfo.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Info:";
+            // 
+            // cbPredmet
+            // 
+            this.cbPredmet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPredmet.FormattingEnabled = true;
+            this.cbPredmet.Location = new System.Drawing.Point(7, 86);
+            this.cbPredmet.Margin = new System.Windows.Forms.Padding(2);
+            this.cbPredmet.Name = "cbPredmet";
+            this.cbPredmet.Size = new System.Drawing.Size(119, 21);
+            this.cbPredmet.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 70);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Predmet:";
+            // 
+            // txtBrojPoruka
+            // 
+            this.txtBrojPoruka.Location = new System.Drawing.Point(7, 42);
+            this.txtBrojPoruka.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBrojPoruka.Name = "txtBrojPoruka";
+            this.txtBrojPoruka.Size = new System.Drawing.Size(119, 20);
+            this.txtBrojPoruka.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Broj poruka:";
+            // 
+            // frmPorukeIB200290
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 466);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnPrintaj);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnNovaPoruka);
+            this.Controls.Add(this.lblStudent);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmPorukeIB200290";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmPorukeIB200290";
+            this.Load += new System.EventHandler(this.frmPorukeIB200290_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentPorukaBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Button btnNovaPoruka;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPrintaj;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbPredmet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBrojPoruka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.BindingSource studentPorukaBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn predmetDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vrijemeKreiranjaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sadrzajDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn slikaDataGridViewImageColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Brisi;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
