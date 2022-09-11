38b3db69f9165209765dae9b3ebfb96d0b1671f4
Adi <efendic100@gmail.com>
2022-09-11 18:38:26 +0200

    Premjestio u rijesenje

---
+++ Ispitni/2022-09-01/Rijesenje/DLWMS.WinForms/DB/Student.cs
@@ -0,0 +1,36 @@
+﻿
+using DLWMS.WinForms.DB;
+using DLWMS.WinForms.IB200290;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace DLWMS.WinForms
+{
+    [Table("Studenti")]
+    public  class Student
+    {
+        KonekcijaNaBazu bp = DLWMSdb.Baza;
+        public int Id { get; set; }
+        public string Indeks { get; set; }
+        public int GodinaStudija { get; set; }
+        // public Image Slika { get; set; }
+        public byte[] Slika { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Email { get; set; }
+        public string Lozinka { get; set; }
+        public bool Aktivan { get; set; }
+        public virtual Spol Spol { get; set; }
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime}";
+        }
+        [NotMapped]
+        public string ImePrezime => $"{Ime} {Prezime}";
+        [NotMapped]
+        public int brojPolozenih => bp.StudentiPredmeti.Where(x => Id == x.Student.Id).Count();
+    }
+}
---
+++ Ispitni/2022-09-01/Rijesenje/DLWMS.WinForms/IB200290/frmNovaPorukaIB200290.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DLWMS.WinForms.IB200290
+{
+    partial class frmNovaPorukaIB200290
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cBPredmet = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSadrzaj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSacuvaj = new System.Windows.Forms.Button();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.err = new System.Windows.Forms.ErrorProvider(this.components);
+            this.pBSlika = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.err)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSlika)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Odaberite predmet:";
+            // 
+            // cBPredmet
+            // 
+            this.cBPredmet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBPredmet.FormattingEnabled = true;
+            this.cBPredmet.Location = new System.Drawing.Point(13, 33);
+            this.cBPredmet.Name = "cBPredmet";
+            this.cBPredmet.Size = new System.Drawing.Size(345, 24);
+            this.cBPredmet.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sadrzaj poruke:";
+            // 
+            // txtSadrzaj
+            // 
+            this.txtSadrzaj.Location = new System.Drawing.Point(16, 94);
+            this.txtSadrzaj.Multiline = true;
+            this.txtSadrzaj.Name = "txtSadrzaj";
+            this.txtSadrzaj.Size = new System.Drawing.Size(342, 295);
+            this.txtSadrzaj.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Slika:";
+            // 
+            // btnSacuvaj
+            // 
+            this.btnSacuvaj.Location = new System.Drawing.Point(676, 405);
+            this.btnSacuvaj.Name = "btnSacuvaj";
+            this.btnSacuvaj.Size = new System.Drawing.Size(111, 38);
+            this.btnSacuvaj.TabIndex = 6;
+            this.btnSacuvaj.Text = "Sacuvaj";
+            this.btnSacuvaj.UseVisualStyleBackColor = true;
+            this.btnSacuvaj.Click += new System.EventHandler(this.btnSacuvaj_Click);
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(528, 405);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(111, 38);
+            this.btnOdustani.TabIndex = 7;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // err
+            // 
+            this.err.ContainerControl = this;
+            // 
+            // pBSlika
+            // 
+            this.pBSlika.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pBSlika.Location = new System.Drawing.Point(397, 33);
+            this.pBSlika.Name = "pBSlika";
+            this.pBSlika.Size = new System.Drawing.Size(390, 356);
+            this.pBSlika.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBSlika.TabIndex = 5;
+            this.pBSlika.TabStop = false;
+            this.pBSlika.DoubleClick += new System.EventHandler(this.pBSlika_Click);
+            // 
+            // frmNovaPorukaIB200290
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 459);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnSacuvaj);
+            this.Controls.Add(this.pBSlika);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSadrzaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cBPredmet);
+            this.Controls.Add(this.label1);
+            this.Name = "frmNovaPorukaIB200290";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmNovaPorukaIB200290";
+            this.Load += new System.EventHandler(this.frmNovaPorukaIB200290_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.err)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSlika)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cBPredmet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSadrzaj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pBSlika;
+        private System.Windows.Forms.Button btnSacuvaj;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.ErrorProvider err;
+    }
+}
